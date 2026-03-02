dc35978e8ad55f8b7098c985e6f27977328fd244
csc27 <85706854+csc27@users.noreply.github.com>
2026-03-01 16:48:43 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ credentials_dict["private_key"] = credentials_dict["private_key"].replace("\\n",
 try:
     credentials = Credentials.from_service_account_info(credentials_dict)
     client = storage.Client(credentials=credentials, project="radiant-moon-488919-k4")
-    bucket = client.get_bucket("beergame2")
+    bucket = client.get_bucket("beergame-cc-streamlit")
 except Exception as exc:
     st.error(f"GCP setup failed: {exc}")
     st.stop()
